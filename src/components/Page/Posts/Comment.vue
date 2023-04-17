d3928aca16f8da9acdbb14f1e15a31d11a1e9c67
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2023-04-17 21:25:00 +0900

    各コンポーネントの作成

---
+++ src/components/Page/Posts/Comment.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+const props = defineProps({
+	comments: {
+		type: Array<{
+			avatar: string
+			name: string
+			date: string
+			comment: string
+		}>,
+		required: true,
+	},
+})
+</script>
+
+<template>
+	<div>
+		<div class="mb-5">
+		  <div v-for="(comment, index) in props.comments" :key="index" class="mb-5 last:mb-0">
+				<div class="flex items-center justify-between mb-1">
+				  <div class="flex items-center">
+						<img
+						  class="w-9 h-9 rounded-full mr-3"
+						  :src="comment.avatar"
+						  alt="Avatar"
+						/>
+						<p class="text-black text-base font-bold">{{ comment.name }}</p>
+				  </div>
+				  <p class="text-base text-gray-400">{{ comment.date }}</p>
+				</div>
+				<p class="text-base text-gray-700">
+					{{ comment.comment }}
+				</p>
+		  </div>
+		</div>
+
+		<div class="flex item-center justify-between">
+		  <input
+				class="px-2 py-2 w-11/12 text-base text-gray-700 border border-gray-400 rounded focus:outline-none focus:border-indigo-500"
+				type="text"
+				placeholder="Want to comment? Comments will be available soon."
+		  />
+		  <button
+				class="p-2 inline-flex items-center justify-center rounded-full shadow-sm cursor-pointer"
+				type="button"
+		  >
+				<img class="w-5 h-5" src="../../../assets/images/icon-send.svg" alt="paper-airplane" />
+		  </button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Page/Posts/Contents.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps({
+	contents: {
+		type: String,
+		required: true,
+	},
+})
+</script>
+
+<template>
+  <div class="mb-5 text-3xl font-bold text-black">
+		{{ props.contents }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
