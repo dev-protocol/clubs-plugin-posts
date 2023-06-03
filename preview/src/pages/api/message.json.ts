81736127811cd1115a2296685daa34de3c32d657
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2023-06-03 15:48:28 +0900

    posting message

---
+++ preview/src/pages/api/message.json.ts
@@ -0,0 +1,12 @@
+import type {APIRoute} from 'astro'
+
+export const post: APIRoute = async ({ request }) => {
+	const body = await request.json();
+	const contents = body.contents;
+
+	return new Response(JSON.stringify({
+		message: contents
+	}), {
+		status: 200
+	})
+}
