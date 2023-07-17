d355e0663a3c071db798e1362fb1f7b28350f777
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2023-07-17 12:28:37 +0900

    fix warning

--- src/components/Page/Index.vue
+++ src/components/Page/Index.vue
@@ -5,9 +5,9 @@ import Contents from './Posts/Contents.vue'
 import Media from './Posts/Media.vue'
 import Comment from './Posts/Comment.vue'
 import type { Option, Posts } from '../../types'
-import {onMounted, ref} from 'vue'
+import { onMounted, ref } from 'vue'
 import Line from '../Common/Line.vue'
-import {decode, encode} from '@devprotocol/clubs-core'
+import { decode, encode } from '@devprotocol/clubs-core'
 
 type Props = {
 	options: Option[]
@@ -36,11 +36,14 @@ let posts = ref<Posts[]>([])
 onMounted(() => {
 	// Postsの取得
 	const params = {
-		hash: "0x100000",
-		sig: "0x200000"
-	};
-	const query = new URLSearchParams(params);
-	const url = new URL(`/api/clubs-plugin-posts/${props.propertyAddress}/message?${query}`, window.location.origin)
+		hash: '0x100000',
+		sig: '0x200000',
+	}
+	const query = new URLSearchParams(params)
+	const url = new URL(
+		`/api/clubs-plugin-posts/${props.propertyAddress}/message?${query}`,
+		window.location.origin,
+	)
 
 	fetch(url.toString())
 		.then(async (res) => {
@@ -50,10 +53,10 @@ onMounted(() => {
 			}
 		})
 		.catch((err) => {
-				console.error(err)
-				error.value = 'Sorry, but there was an error loading the timeline. Please try again later.'
-			}
-		)
+			console.error(err)
+			error.value =
+				'Sorry, but there was an error loading the timeline. Please try again later.'
+		})
 		.finally(() => {
 			isLoading.value = false
 		})
@@ -72,7 +75,9 @@ onMounted(() => {
 
 		<!-- Loading -->
 		<div v-if="isLoading" class="flex justify-center mb-5 p-5 rounded bg-white">
-	  	<div class="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+			<div
+				class="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"
+			></div>
 		</div>
 
 		<!-- Error -->
@@ -81,8 +86,13 @@ onMounted(() => {
 		</div>
 
 		<!-- Timeline empty -->
-		<div v-if="!isLoading && posts.length === 0" class="mb-5 p-5 rounded bg-white">
-			<p class="text-center">Sorry, but there are no posts on this timeline yet</p>
+		<div
+			v-if="!isLoading && posts.length === 0"
+			class="mb-5 p-5 rounded bg-white"
+		>
+			<p class="text-center">
+				Sorry, but there are no posts on this timeline yet
+			</p>
 		</div>
 
 		<!-- Timeline -->
@@ -104,7 +114,7 @@ onMounted(() => {
 					:date="post.created_at"
 					:contents="post.content"
 				/>
-				<Reactions likes="100" />
+				<Reactions :likes="100" />
 				<Media :images="post.options.find((item) => item.key === '#images')" />
 				<Line class="mb-5" />
 				<Comment
@@ -121,7 +131,7 @@ onMounted(() => {
 					:contents="post.content"
 				/>
 				<Media :required="true" />
-				<Reactions likes="100" />
+				<Reactions :likes="100" />
 			</div>
 		</article>
 	</div>
--- src/components/Page/Posts/Contents.vue
+++ src/components/Page/Posts/Contents.vue
@@ -1,34 +1,33 @@
 <script lang="ts" setup>
-import Header from './Header.vue';
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'
 
 type Props = {
-	name: string;
-	avatar: string;
-	date: string;
-	contents: string;
-};
+	name: string
+	avatar: string
+	date: Date
+	contents: string
+}
 
 const props = defineProps<Props>()
 </script>
 
 <template>
-  <div class="flex items-center justify-between mb-1">
+	<div class="flex items-center justify-between mb-1">
 		<div class="flex items-center">
-	  	<img
+			<img
 				class="w-12 h-12 rounded-full mr-3"
 				:src="props.avatar"
 				alt="Avatar of Aggre"
-	  	/>
-	  	<p class="text-black text-base font-bold">{{ props.name }}</p>
+			/>
+			<p class="text-black text-base font-bold">{{ props.name }}</p>
 		</div>
-		<p class="text-base text-gray-400">{{ dayjs(props.date).format('d MMM H:mm') }}</p>
-  </div>
-  <div class="mb-5 text-3xl font-bold text-black">
+		<p class="text-base text-gray-400">
+			{{ dayjs(props.date).format('d MMM H:mm') }}
+		</p>
+	</div>
+	<div class="mb-5 text-3xl font-bold text-black">
 		{{ props.contents }}
-  </div>
+	</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
