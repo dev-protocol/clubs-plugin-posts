5c7c938a3ef8c3f50ee47aa8198a7e4e32cd4c90
iyashragrawal <yash@devprotocol.xyz>
2023-07-28 12:27:37 +0530

    fix: add proper loading of env variables

--- preview/astro.config.mjs
+++ preview/astro.config.mjs
@@ -1,8 +1,12 @@
+import { config } from 'dotenv'
 import { defineConfig } from 'astro/config'
 import clubs from '@devprotocol/clubs-core'
 import vue from '@astrojs/vue'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
+
+// eslint-disable-next-line functional/no-expression-statement
+config({ path: './.env'});
 
 export default defineConfig({
 	server: {
