6d28a9c82a0d031fbd90a98c41e4e4677552ec06
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2023-08-26 16:59:33 +0900

    fix profile

---
+++ src/components/Common/Profile.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { Avatar } from '@boringer-avatars/vue3'
+import { onMounted, ref } from 'vue'
+import { ZeroAddress } from 'ethers'
+
+type Props = {
+	propertyAddress: string
+	address: string
+}
+
+const props = defineProps<Props>()
+
+const avatar = ref('')
+const name = ref('')
+
+onMounted(() => {
+	if (!props.address || props.address === ZeroAddress) {
+		name.value = props.address
+		return
+	}
+
+	// fetch profile
+	fetchProfile(props.address)
+})
+
+const fetchProfile = async (address: string) => {
+	const url = new URL(
+		`/api/clubs-plugin-posts/${props.propertyAddress}/profile?address=${address}`,
+		window.location.origin,
+	)
+
+	return fetch(url.toString())
+		.then(async (res) => {
+			if (res.status !== 200) return
+
+			const json = await res.json()
+
+			avatar.value = json.profile.avatar
+			name.value = json.profile.username ?? address
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+}
+</script>
+
+<template>
+	<div class="flex items-center w-9/12">
+		<template v-if="avatar">
+			<div
+				class="w-12 h-12 rounded-full mr-3 bg-center bg-cover bg-no-repeat"
+				:style="`background-image: url(${avatar})`"
+			/>
+		</template>
+		<template v-else>
+			<Avatar
+				class="mr-3"
+				:title="false"
+				:size="48"
+				variant="beam"
+				:square="false"
+			/>
+		</template>
+		<p class="text-black text-base font-bold overflow-hidden overflow-ellipsis">
+			{{ name }}
+		</p>
+	</div>
+</template>
