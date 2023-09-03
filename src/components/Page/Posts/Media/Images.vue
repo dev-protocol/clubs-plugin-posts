3c921e20458fdefec169a8ec465fcb7901208b58
aggre <hiroyuki.aggre@gmail.com>
2023-09-04 07:49:06 +0900

    impl: Image viewer

--- src/components/Page/Posts/Media/Images.vue
+++ src/components/Page/Posts/Media/Images.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import type { Ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
+import PhotoSwipeLightbox from 'photoswipe/lightbox'
+import 'photoswipe/style.css'
+import { solidityPackedKeccak256 } from 'ethers'
+import { always } from 'ramda'
 
 type Props = {
 	images: string[]
-	isPost: {
+	isPost?: {
 		type: BooleanConstructor
 		default: false
 	}
+}
+type ImageData = {
+	src: string
+	w: number
+	h: number
+	alt: string
 }
 
 interface Emits {
@@ -14,46 +24,100 @@ interface Emits {
 }
 
 const { images } = defineProps<Props>()
+const galleryImages = ref<ImageData[]>()
+const galleryId = computed<string>(() =>
+	solidityPackedKeccak256(images.map(always('string')), images).replace(
+		/[0-9]/g,
+		'',
+	),
+)
+
 const emit = defineEmits<Emits>()
 const handleDeleteImage = (index: number) => {
 	emit('delete:image', index)
 }
+
+let psl: typeof PhotoSwipeLightbox
+
+const initPsl = async () => {
+	galleryImages.value = await Promise.all(
+		images.map(async (src) => {
+			const img = await new Promise<ImageData>((res) => {
+				const _img = new Image()
+				_img.onload = () =>
+					res({ src: _img.src, w: _img.width, h: _img.height, alt: _img.alt })
+				_img.src = src
+			})
+			return img
+		}),
+	)
+
+	psl && psl.destroy()
+	psl = new PhotoSwipeLightbox({
+		gallery: `#${galleryId.value}`,
+		children: 'a',
+		pswpModule: () => import('photoswipe'),
+	})
+	psl.init()
+}
+
+onMounted(() => {
+	initPsl()
+})
+
+watch(images, () => {
+	initPsl()
+})
 </script>
 
 <template>
-	<div
-		v-if="images"
-		v-for="(image, index) in images"
-		:key="image"
-		class="relative bg-gray-100 overflow-hidden"
-		:style="{ width: images.length === 1 ? '100%' : 'calc(50% - 2px)' }"
-	>
-		<!-- image -->
-		<img class="w-full rounded" :src="image" alt="Nature" />
-		<!-- /image -->
-		<!-- delete media -->
-		<div
-			v-if="isPost"
-			class="rounded-full absolute top-2 right-2 w-8 h-8 flex items-center justify-center cursor-pointer"
-			aria-label="delete media"
-			role="button"
-			style="background-color: rgba(15, 20, 25, 0.75)"
-			@click="handleDeleteImage(index)"
+	<div :id="galleryId" class="flex flex-wrap gap-y-1 gap-x-1">
+		<span
+			v-if="galleryImages"
+			v-for="(image, index) in galleryImages"
+			:style="{ width: images.length === 1 ? '100%' : 'calc(50% - 2px)' }"
+			:key="image.src + image.w + image.h"
+			class="relative bg-gray-100 overflow-hidden"
 		>
-			<svg
-				viewBox="0 0 24 24"
-				aria-hidden="true"
-				class="w-4 h-4 text-white fill-current"
+			<a
+				:data-pswp-width="image.w"
+				:data-pswp-height="image.h"
+				:href="image.src"
+				target="_blank"
+				rel="noreferrer"
 			>
-				<g>
-					<path
-						class="text-white"
-						d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"
-					></path>
-				</g>
-			</svg>
-		</div>
-		<!-- /delete media -->
+				<!-- image -->
+				<img
+					class="w-full h-full rounded object-cover"
+					:src="image.src"
+					:alt="image.alt"
+				/>
+				<!-- /image -->
+			</a>
+			<!-- delete media -->
+			<div
+				v-if="isPost"
+				class="rounded-full absolute top-2 right-2 w-8 h-8 flex items-center justify-center cursor-pointer"
+				aria-label="delete media"
+				role="button"
+				style="background-color: rgba(15, 20, 25, 0.75)"
+				@click="handleDeleteImage(index)"
+			>
+				<svg
+					viewBox="0 0 24 24"
+					aria-hidden="true"
+					class="w-4 h-4 text-white fill-current"
+				>
+					<g>
+						<path
+							class="text-white"
+							d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"
+						></path>
+					</g>
+				</svg>
+			</div>
+			<!-- /delete media -->
+		</span>
 	</div>
 </template>
 
