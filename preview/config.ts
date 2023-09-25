732d94c7a5bad4fda2a1b061ff197fe6f6932d14
aggre <hiroyuki.aggre@gmail.com>
2023-09-25 12:34:57 +0900

    change rpcUrl for preview

--- preview/config.ts
+++ preview/config.ts
@@ -38,7 +38,7 @@ export default () =>
 		url: '',
 		propertyAddress: '0xE59fEDaBB0F79b0EC605737805a9125cd8d87B1f',
 		chainId: 80001,
-		rpcUrl: 'https://rpc-mumbai.matic.today/',
+		rpcUrl: 'https://polygon-mumbai-bor.publicnode.com',
 		adminRolePoints: 50,
 		plugins: [
 			{
