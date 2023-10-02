81ee507c54e4cccc3c8df1562f4135c913a5b987
aggre <hiroyuki.aggre@gmail.com>
2023-10-02 15:25:06 +0900

    tweak

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -6,7 +6,9 @@ import Icon from '../../assets/images/plugin-icon.svg'
 	<div class="container mx-auto px-4">
 		<h1 class="text-3xl font-bold mb-4">Posts</h1>
 
-		<div class="rounded-md min-h-2xl p-3 bg-dp-blue-grey-300 grid gap-3">
+		<div
+			class="rounded-md justify-items-center w-max mx-auto min-h-2xl p-3 bg-dp-blue-grey-300 grid gap-3"
+		>
 			<p>There's nothing here yet, but will be more fun things to come...</p>
 			<img
 				class="rounded-md opacity-20"
