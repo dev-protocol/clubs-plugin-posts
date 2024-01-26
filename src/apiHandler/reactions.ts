9ceb879fe7caecd51d7c1ed07a0aaf5be7c4e14e
aggre <hiroyuki.aggre@gmail.com>
2024-01-27 07:06:24 +0900

    Flexible reactions (#694)
    * `reactions` is now open for all value
    
    * rename API parameter
    
    * fix Props
    
    * remove unused imports
    
    * 0.9.0

--- src/apiHandler/reactions.ts
+++ src/apiHandler/reactions.ts
@@ -1,24 +1,23 @@
 import { verifyMessage } from 'ethers'
 import type { ClubsConfiguration } from '@devprotocol/clubs-core'
-import { emojiAllowList } from '../constants'
 import { addReactionDocumentsRedis } from './reactions-documents-redis'
 
 export type AddReactionRequestJson = Readonly<{
 	readonly hash: string
 	readonly sig: string
 	readonly postId: string
-	readonly emoji: string
+	readonly data: string
 }>
 
 export const addReactionHandler =
 	(conf: ClubsConfiguration, dbQueryKey: string) =>
 	async ({ request }: { readonly request: Request }) => {
-		const { hash, sig, postId, emoji } =
+		const { hash, sig, postId, data } =
 			(await request.json()) as AddReactionRequestJson
-		if (!emoji || !hash || !sig || !postId) {
+		if (!data || !hash || !sig || !postId) {
 			return new Response(
 				JSON.stringify({
-					error: 'Missing data',
+					error: 'Missing required content',
 				}),
 				{
 					status: 400,
@@ -27,33 +26,38 @@ export const addReactionHandler =
 		}
 
 		/**
-		 * Check if club has allowListEmojis option
-		 * If not, use default allowListEmojis
+		 * Emoji validation is now disabled.
+		 * Reactions can also be used for voting, not only showing emotes.
+		 * Also, restrictions on displayable emojis as reactions are doing on Reactions.vue.
 		 */
-		const clubAllowListEmojis =
-			(conf.options?.find((item) => item.key === 'allowListEmojis')
-				?.value as readonly string[]) ?? emojiAllowList
+		// /**
+		//  * Check if club has allowListEmojis option
+		//  * If not, use default allowListEmojis
+		//  */
+		// const clubAllowListEmojis =
+		// 	(conf.options?.find((item) => item.key === 'allowListEmojis')
+		// 		?.value as readonly string[]) ?? emojiAllowList
 
-		/**
-		 * Only allowlist emojis accepted
-		 */
-		if (!clubAllowListEmojis.includes(emoji)) {
-			return new Response(
-				JSON.stringify({
-					error: 'Emoji not allowed',
-				}),
-				{
-					status: 400,
-				},
-			)
-		}
+		// /**
+		//  * Only allowlist emojis accepted
+		//  */
+		// if (!clubAllowListEmojis.includes(emoji)) {
+		// 	return new Response(
+		// 		JSON.stringify({
+		// 			error: 'Emoji not allowed',
+		// 		}),
+		// 		{
+		// 			status: 400,
+		// 		},
+		// 	)
+		// }
 
 		// get user address
 		const userAddress = verifyMessage(hash, sig)
 
 		return addReactionDocumentsRedis({
 			conf,
-			data: emoji,
+			data,
 			userAddress,
 			postId,
 			dbQueryKey,
