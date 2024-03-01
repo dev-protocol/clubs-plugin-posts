1c3ce617ffa4d8ed3031afcfbc411829c520aa30
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2024-03-01 15:31:21 +0900

    Implement error handling for feed slug input
    Added checks for empty or duplicate slug values in the feed creation and editing process. The changes involve indicating an error state and displaying appropriate error messages in the NewFeed.vue and EditFeed.vue components. This improves the user's understanding of input requirements and enhances the overall application robustness.

--- src/components/Admin/EditFeed.vue
+++ src/components/Admin/EditFeed.vue
@@ -33,6 +33,8 @@ const props = defineProps({
 const currentPluginIndex = ref(props.clubs?.currentPluginIndex || 0)
 const slug = ref(props.slug)
 const title = ref(props.title)
+const isSlugError = ref(false)
+const errorMessage = ref('')
 
 const onChange = () => {
 	if (currentPluginIndex.value === 0) {
@@ -40,12 +42,25 @@ const onChange = () => {
 	}
 
 	if (slug.value === '') {
+		isSlugError.value = true
+		errorMessage.value = 'slug is required'
 		return
 	}
+	isSlugError.value = false
 
 	let feeds: OptionsDatabase[] =
 		props.options.find(({ key }: { key: string }) => key === 'feeds')?.value ||
 		[]
+
+	if (
+		props.slug !== slug.value &&
+		feeds.some((feed) => feed.slug === slug.value)
+	) {
+		isSlugError.value = true
+		errorMessage.value = 'slug is already exists'
+		return
+	}
+	isSlugError.value = false
 
 	feeds = feeds.map((feed) => {
 		if (feed.id === props.feedId) {
@@ -61,19 +76,37 @@ const onChange = () => {
 	setOptions([{ key: 'feeds', value: feeds }], currentPluginIndex.value)
 }
 </script>
+<style scoped>
+.hs-form-field {
+	.hs-form-field__helper,
+	.hs-form-field__label {
+		&.label__error {
+			color: #c92020;
+		}
+	}
+}
+</style>
 <template>
 	<div class="py-4">
 		<h1 class="mb-2 text-3xl font-bold">Edit Feeds</h1>
 		<p class="mb-6">You can change the settings of the Feed</p>
 		<div class="flex flex-col gap-2">
 			<label class="hs-form-field is-filled flex flex-col">
-				<span class="hs-form-field__label"> Slug </span>
+				<span
+					class="hs-form-field__label"
+					:class="{ label__error: isSlugError }"
+				>
+					Slug
+				</span>
 				<input
 					v-model="slug"
 					class="hs-form-field__input"
 					placeholder="slug"
 					@change="onChange"
 				/>
+				<span v-if="isSlugError" class="hs-form-field__helper label__error">
+					{{ errorMessage }}
+				</span>
 			</label>
 
 			<label class="hs-form-field is-filled flex flex-col">
--- src/components/Admin/NewFeed.vue
+++ src/components/Admin/NewFeed.vue
@@ -23,6 +23,8 @@ const currentPluginIndex = ref(props.clubs?.currentPluginIndex || 0)
 
 const slug = ref('')
 const title = ref('')
+const isSlugError = ref(false)
+const errorMessage = ref('')
 
 const onChange = () => {
 	if (currentPluginIndex.value === 0) {
@@ -30,18 +32,22 @@ const onChange = () => {
 	}
 
 	if (slug.value === '') {
+		isSlugError.value = true
+		errorMessage.value = 'slug is required'
 		return
 	}
+	isSlugError.value = false
 
 	let feeds: OptionsDatabase[] =
 		props.options.find(({ key }: { key: string }) => key === 'feeds')?.value ||
 		[]
 
-	// feedsに同じslugがある場合は登録できない
-	// Todo: 引っかかった時の処理を追加する
 	if (feeds.some((feed) => feed.slug === slug.value)) {
+		isSlugError.value = true
+		errorMessage.value = 'slug is already exists'
 		return
 	}
+	isSlugError.value = false
 
 	const uuid = uuidFactory(props.url)
 	feeds = feeds.concat([
@@ -59,19 +65,37 @@ const onChange = () => {
 	setOptions([{ key: 'feeds', value: feeds }], currentPluginIndex.value)
 }
 </script>
+<style scoped>
+.hs-form-field {
+	.hs-form-field__helper,
+	.hs-form-field__label {
+		&.label__error {
+			color: #c92020;
+		}
+	}
+}
+</style>
 <template>
 	<div class="py-4">
 		<h1 class="mb-2 text-3xl font-bold">Create New Feeds</h1>
 		<p class="mb-6">Please enter the following to add a new feed</p>
 		<div class="flex flex-col gap-2">
 			<label class="hs-form-field is-filled flex flex-col">
-				<span class="hs-form-field__label"> Slug </span>
+				<span
+					class="hs-form-field__label"
+					:class="{ label__error: isSlugError }"
+				>
+					Slug
+				</span>
 				<input
 					v-model="slug"
 					class="hs-form-field__input"
 					placeholder="slug"
 					@change="onChange"
 				/>
+				<span v-if="isSlugError" class="hs-form-field__helper label__error">
+					{{ errorMessage }}
+				</span>
 			</label>
 
 			<label class="hs-form-field is-filled flex flex-col">
