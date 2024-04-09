79fb7b76d59f32b060f17f54955ab9c3456fbee2
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-04-09 11:21:57 +0900

    adds localization for posts components

---
+++ src/components/Page/Posts/i18n/index.ts
@@ -0,0 +1,9 @@
+import type { ClubsI18nParts } from '@devprotocol/clubs-core'
+
+export const Strings = {
+	Post: { en: 'Post', ja: '' },
+	Title: { en: 'Title', ja: '' },
+	WhatsHappening: { en: "What's happening", ja: '' },
+	LimitedAccess: { en: 'Limited access', ja: '' },
+	UnlockInstructions: { en: 'Unlock this post by becoming a member', ja: '' },
+} satisfies ClubsI18nParts
