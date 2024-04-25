61717ca9b8326b6c5b0739e212e47496946d3410
Aggre <hiroyuki.aggre@gmail.com>
2024-04-25 17:35:57 +0900

    fix too-many-connections for redis on getSlots

--- src/index.ts
+++ src/index.ts
@@ -440,35 +440,42 @@ export const getSlots = (async (options, __, { paths, factory }) => {
 	const feeds =
 		(options.find(({ key }) => key === 'feeds')
 			?.value as readonly OptionsDatabase[]) ?? []
+	const feedsHasPageContentHomeBeforeContent = feeds.filter(
+		(feed) =>
+			feed.slots?.[ClubsSlotName.PageContentHomeBeforeContent] !== undefined &&
+			feed.slots[ClubsSlotName.PageContentHomeBeforeContent].enabled,
+	)
+	const slotsForPageContentHomeBeforeContent =
+		feedsHasPageContentHomeBeforeContent.length > 0
+			? await (async () => {
+					const client = await getDefaultClient()
+					const contents = await Promise.all(
+						feedsHasPageContentHomeBeforeContent.map(async (feed) => {
+							const posts = await getAllPosts({
+								key: feed.database.key,
+							})
+							return [
+								{
+									slot: ClubsSlotName.PageContentHomeBeforeContent,
+									component: News,
+									props: {
+										posts: isNotError(posts) ? posts.map(mask) : [],
+										feedId: feed.id,
+										slots: feed.slots,
+									},
+								},
+							]
+						}),
+					)
+					// eslint-disable-next-line functional/no-expression-statement
+					await client.quit()
+					return contents.flat()
+				})()
+			: []
 
 	return [
 		...(factory === 'page' // == The public feed page with PageContentHomeBeforeContent slot
-			? (
-					await Promise.all(
-						feeds
-							.filter(
-								(feed) =>
-									feed.slots?.[ClubsSlotName.PageContentHomeBeforeContent] !==
-									undefined,
-							)
-							.map(async (feed) => {
-								const posts = await getAllPosts({
-									key: feed.database.key,
-								})
-								return [
-									{
-										slot: ClubsSlotName.PageContentHomeBeforeContent,
-										component: News,
-										props: {
-											posts: isNotError(posts) ? posts.map(mask) : [],
-											feedId: feed.id,
-											slots: feed.slots,
-										},
-									},
-								]
-							}),
-					)
-				).flat()
+			? slotsForPageContentHomeBeforeContent
 			: []),
 		...(factory === 'admin' && path1 === 'posts' && path2 === undefined // == The feeds list page
 			? [
