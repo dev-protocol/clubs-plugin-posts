04fa64e98d0ae97e8e1b9c75c98da33c4f7007fc
Aggre <hiroyuki.aggre@gmail.com>
2024-04-26 13:36:07 +0900

    reduce db connections

--- src/index.ts
+++ src/index.ts
@@ -440,44 +440,33 @@ export const getSlots = (async (options, __, { paths, factory }) => {
 	const feeds =
 		(options.find(({ key }) => key === 'feeds')
 			?.value as readonly OptionsDatabase[]) ?? []
-	const feedsHasPageContentHomeBeforeContent = feeds.filter(
+	const newsFeeds = feeds.filter(
 		(feed) =>
 			feed.slots?.[ClubsSlotName.PageContentHomeBeforeContent] !== undefined &&
 			feed.slots[ClubsSlotName.PageContentHomeBeforeContent].enabled,
 	)
-	const slotsForPageContentHomeBeforeContent =
-		feedsHasPageContentHomeBeforeContent.length > 0
-			? await (async () => {
-					const client = await getDefaultClient()
-					const contents = await Promise.all(
-						feedsHasPageContentHomeBeforeContent.map(async (feed) => {
-							const posts = await getPaginatedPosts({
-								scope: feed.database.key,
-								client,
-								page: 0,
-							})
-							return [
-								{
-									slot: ClubsSlotName.PageContentHomeBeforeContent,
-									component: News,
-									props: {
-										posts: isNotError(posts) ? posts.map(mask) : [],
-										feedId: feed.id,
-										slots: feed.slots,
-									},
+	const newsSlots =
+		newsFeeds.length > 0 && factory === 'page'
+			? newsFeeds
+					.map((feed) => {
+						return [
+							{
+								slot: ClubsSlotName.PageContentHomeBeforeContent,
+								component: News,
+								props: {
+									scope: feed.database.key,
+									feedId: feed.id,
+									slots: feed.slots,
 								},
-							]
-						}),
-					)
-					// eslint-disable-next-line functional/no-expression-statement
-					await client.quit()
-					return contents.flat()
-				})()
+							},
+						]
+					})
+					.flat()
 			: []
 
 	return [
 		...(factory === 'page' // == The public feed page with PageContentHomeBeforeContent slot
-			? slotsForPageContentHomeBeforeContent
+			? newsSlots
 			: []),
 		...(factory === 'admin' && path1 === 'posts' && path2 === undefined // == The feeds list page
 			? [
