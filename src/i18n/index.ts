30838fb7780b20b71c610500819bb593e7166312
Kent / Kuro-chan <89888685+kuro-chan-dev@users.noreply.github.com>
2024-05-21 10:36:35 +0900

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,11 +3,11 @@ import type { ClubsI18nParts } from '@devprotocol/clubs-core'
 export const Strings = {
 	NoPostsOnTimeline: {
 		en: 'Sorry, but there are no posts on this timeline yet.',
-		ja: '申し訳ありませんが、このタイムラインにはまだ投稿がありません。',
+		ja: '申し訳ありません。このタイムラインにはまだ投稿がありません',
 	},
 	NoPostsOnTimelineTryAgain: {
 		en: 'Sorry, but there was an error loading the timeline. Please try again later.',
-		ja: '申し訳ありませんが、タイムラインの読み込み中にエラーが発生しました。後でもう一度試してください。',
+		ja: '申し訳ありません。タイムラインの読み込み中にエラーが発生しました。しばらく経ってから、もう一度試してください。',
 	},
 	SignIn: {
 		en: 'Sign in',
